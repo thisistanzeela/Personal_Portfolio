d933e740765483e125a7e38120f65b9066e80ead
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-11-03 19:55:08 +0530

    Update ContactElements.js

--- src/components/Contact/ContactElements.js
+++ src/components/Contact/ContactElements.js
@@ -28,28 +28,28 @@ export const Email = styled.div`
     }
   }
 `;
-export const MobileNumber = styled.div`
-margin-top:2rem;
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  column-gap:13rem;
+// export const MobileNumber = styled.div`
+// margin-top:2rem;
+//   display: flex;
+//   align-items: center;
+//   flex-direction: row;
+//   justify-content: space-around;
+//   flex-wrap: wrap;
+//   column-gap:13rem;
 
-  span {
-    font-size: 1rem;
-    font-weight: 400;
-  }
+//   span {
+//     font-size: 1rem;
+//     font-weight: 400;
+//   }
 
-  @media (min-width: 576px) {
-    span {
-      font-size: 1.25rem;
-    }
-  }
-  @media (min-width: 992px) {
-    span {
-      font-size: 1.5rem;
-    }
-  }
-`;
+//   @media (min-width: 576px) {
+//     span {
+//       font-size: 1.25rem;
+//     }
+//   }
+//   @media (min-width: 992px) {
+//     span {
+//       font-size: 1.5rem;
+//     }
+//   }
+// `;
